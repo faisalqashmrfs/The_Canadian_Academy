377cdee4ec497995b8aea4ad7fb1a77b024f8cce
Faisal qashmr <faisalqashmr.fs@gmail.com>
2025-10-16 14:36:17 +0300

    mearge

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -1,10 +1,12 @@
 import Hero from '../../Components/Hero/Hero'
+import OurStory from '../../Components/OurStory/OurStory'
 import './AboutUs.css'
 
 export default function AboutUs() {
   return (
     <div>
         <Hero title1={"Building Futures"} title2={"Through Education"} p={"At The Canadian Academy, we believe education goes beyond classrooms. Our inclusive learning environment and dedicated instructors help students build knowledge, skills, and confidence to succeed in academics, careers, and life. What makes us unique is our commitment to support every learner and celebrate diverse paths to success."}/>
+        <OurStory />
     </div>
   )
 }
